9d913966ca70e49b126bd2a92c60b7151fa317ba
Yu Xuan <yu_xuan_tan@hotmail.com>
2023-02-15 21:44:54 +0800

    use env secrets for gsheets

--- gsheet_controller.py
+++ gsheet_controller.py
@@ -1,12 +1,18 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import os
+import json
 
 # use creds to create a client to interact with the Google Drive API  
 class GsheetController():
     def __init__(self, gsheet_name = "Test Financial Status"):
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-        creds = ServiceAccountCredentials.from_json_keyfile_name('google_creds.json', scope)
+        try:
+            gsheet_creds_dict = json.loads(os.environ["GSHEET_CREDENTIALS"])
+            creds = ServiceAccountCredentials.from_json_keyfile_dict(gsheet_creds_dict, scope)
+        except KeyError:
+            creds = ServiceAccountCredentials.from_json_keyfile_name('google_creds.json', scope)
         client = gspread.authorize(creds)
         self._sh = client.open(gsheet_name)
 
