23d53f39e6addc372ddf78c7743feec07cd3081e
YX <yx@YXs-MacBook-Air.local>
2024-09-12 00:45:06 +0800

    update code to portfolio aggregator

--- mysite/configs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('get_recs/', views.get_recs, name='get_recs'),
-    path('send_tele/', views.send_tele, name='send_tele'),
-]
